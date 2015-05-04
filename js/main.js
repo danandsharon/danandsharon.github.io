e98d09bcdc23cc0c28830890706a0b110e641649
Stephanie <stephaniexyeung@gmail.com>
2015-05-03 18:33:25 -0700

    f semicolons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
-  var today = Date.now();
-  var weddingDate = Date.UTC(2015, 08, 19);
-  var msUntil = weddingDate - today;
+  var today = Date.now()
+  var weddingDate = Date.UTC(2015, 08, 19)
+  var msUntil = weddingDate - today
   var daysUntil = Math.floor(msUntil / (24 * 60 * 60 * 1000))
 
   $('.days')[0].innerText = daysUntil
