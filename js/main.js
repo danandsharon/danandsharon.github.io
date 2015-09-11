b7a1f2005c39c9ba9019c6611244e2030f61cc34
Stephanie <stephaniexyeung@gmail.com>
2015-09-10 21:44:03 -0700

    Ceil

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
   var today = Date.now()
   var weddingDate = Date.UTC(2015, 08, 19)
   var msUntil = weddingDate - today
-  var daysUntil = Math.floor(msUntil / (24 * 60 * 60 * 1000))
+  var daysUntil = Math.ceil(msUntil / (24 * 60 * 60 * 1000))
 
   $('.days')[0].innerText = daysUntil
 })
